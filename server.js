11e8cd93eb775d44199e2f1bdf626048bc7c3eb1
Chaitany-exe <jeetkrishna3333@gmail.com>
2024-04-26 13:58:02 +0530

    origin

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server,{
     cors:{
-        origin:"https://originaltertiary.com/",
+        origin:"*",
         methods:"*"
     }
 });
